6210c377d0df985cf9d73863eb173d9bf0349afe
Sylvain Boulade <bopulie@gmail.com>
2017-06-21 21:51:44 +0200

    Setup the lyric list and cleanup

--- client/components/LyricList.js
+++ client/components/LyricList.js
@@ -1,15 +1,34 @@
 import React from "react";
+import { graphql } from "react-apollo";
+import likeLyric from "../mutations/likeLyric";
 
 class LyricList extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.likeLyric = this.likeLyric.bind(this);
+  }
+
+  likeLyric(id) {
+    console.log(id);
+    // this.props
+    //   .mutate({
+    //     variables: { id }
+    //   })
+  }
+
   render() {
     return (
       <ul className="collection">
-        {this.props.lyrics.map(lyric =>
-          <li key={lyric.id} className="collection-item">{lyric.content}</li>
+        {this.props.lyrics.map(({ id, content }) =>
+          <li key={id} className="collection-item">
+            {content}
+            <i className="material-icons" onClick={() => this.likeLyric(id)}>thumb_up</i>
+          </li>
         )}
       </ul>
     );
   }
 }
 
-export default LyricList;
+export default graphql(likeLyric)(LyricList);
